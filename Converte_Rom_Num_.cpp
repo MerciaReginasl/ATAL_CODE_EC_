7aa6b759651ebbca2d18ad86ac9528690ed09565
Mércia Regina da Silva <39158160+MerciaReginasl@users.noreply.github.com>
2023-07-28 19:40:41 -0300

    Add files via upload

--- Converte_Rom_Num_.cpp
+++ Converte_Rom_Num_.cpp
@@ -3,40 +3,39 @@
 #include <string.h>
 #include <math.h>
 
-int romanToDecimal(char roman) {
-    switch (roman) {
+int Romano_para_Decimal(char romano) {
+    switch (romano) {
         case 'I': return 1;
         case 'V': return 5;
         case 'X': return 10;
         case 'L': return 50;
         case 'C': return 100;
-        case 'D': return 500;
-        case 'M': return 1000;
+        
         default: return 0;
     }
 }
 
 int main() {
-    char romanNumber[10];
+    char romano_Number[100];
     printf("Digite um número romano: ");
-    scanf("%s", romanNumber);
+    scanf("%s", romano_Number);
 
-    int decimalNumber = 0;
+    int decimal_Number = 0;
     int i;
 
-    for (i = 0; romanNumber[i] != '\0'; i++) {
-        int currentDigit = romanToDecimal(romanNumber[i]);
-        int nextDigit = romanToDecimal(romanNumber[i + 1]);
+    for (i = 0; romano_Number[i] != '\0'; i++) {
+        int Atual_Number = Romano_para_Decimal(romano_Number[i]);
+        int Proximo_Digito = Romano_para_Decimal(romano_Number[i + 1]);
 
-        if (nextDigit > currentDigit) {
-            decimalNumber += nextDigit - currentDigit;
+        if (Proximo_Digito > Atual_Number) {
+            decimal_Number += Proximo_Digito - Atual_Number;
             i++; // Avança para o próximo caractere do número romano
         } else {
-            decimalNumber += currentDigit;
+            decimal_Number += Atual_Number;
         }
     }
 
-    printf("O número decimal equivalente é: %d\n", decimalNumber);
+    printf("O número decimal equivalente é: %d\n", decimal_Number);
 
     return 0;
 }
